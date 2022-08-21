fcbccccd84480180f3fe04b9a65ea8d1cfead2b9
paulohmoreira <phmoreira37@gmail.com>
2022-08-21 15:46:43 -0300

    Adicionado pular com as setas e função de level up

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const dino = document.querySelector(".dino");
 const background = document.querySelector(".background");
 let isJumping = false;
 let position = 0;
+let score = 0;
+let positionLevel = 6;
 
 //Função para identificar o clique no botão 'espaço' do teclado
 function handleKeyUp(event) {
   //Código gerado ao pressionar espaço = 32
-  if (event.keyCode === 32) {
+  if (event.keyCode === 32 || event.keyCode === 38) {
     if (!isJumping) {
       jump();
     }
@@ -49,20 +51,29 @@ function createCactus() {
   cactus.style.left = 1000 + "px";
   // adiciona o cactus criado na div background que está no nosso documento html
   background.appendChild(cactus);
-
-  let leftInterval = setInterval(() => {
+  let leftTimer = setInterval(() => {
     if (cactusPosition < -60) {
-      clearInterval(leftInterval);
+      clearInterval(leftTimer);
       background.removeChild(cactus);
+      score += 10;
+      document.getElementById("score").innerHTML = "Score: " + score;
     } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
-      clearInterval(leftInterval);
-      document.body.innerHTML = '<h1 class="game-over">Fim de Jogo</h1>';
+      clearInterval(leftTimer);
+      document.body.innerHTML =
+        '<h1 class="game-over">Fim de Jogo</h1><p>Você fez ' +
+        score +
+        ' pontos.</p><a class="restart" href="javascript:window.location.reload(true)"><p>Clique aqui para jogar novamente</p></a>';
     } else {
-      cactusPosition -= 8;
+      cactusPosition -= positionLevel;
       cactus.style.left = cactusPosition + "px";
     }
   }, 20);
+  setTimeout(levelUp, 10000);
   setTimeout(createCactus, ramdomTime);
+}
+
+function levelUp() {
+  positionLevel += 1;
 }
 
 createCactus();
